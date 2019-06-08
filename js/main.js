bef65fb9db81ec056023a8557e43451ae8712118
molrobot <john19990708@gmail.com>
2019-06-08 17:26:52 +0800

    Fixed
    Fixed Headsign
    
    Decrease network traffic
    
    Fuzzy search route name

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 var Data, City, RouteName;
+//JSON資料、城市名稱、路線名稱
 
+//網頁載入執行
 $(function () {
     data = '';
     City = '';
     RouteName = '';
     
+    //下拉式選單新增城市名稱
     for(var key in CityList){
         $("#SelectCity").append('<option value="' + CityList[key] + '">' + key + "</option>");
     }
     
+    //下拉式選單改變事件
     $("#SelectCity").on("change", function() {
         City = $(this).val();
         $("input#RouteName").val("");
@@ -17,11 +21,11 @@ $(function () {
         getRouteData();
     });
     
+    //輸入事件
     $("input#RouteName").keyup(function() {
         RouteName = $(this).val();
-        $("#RouteData").empty();
         if(City != "")
-            getRouteData();
+            searchRouteData();
     });
     
     /*
@@ -35,7 +39,7 @@ $(function () {
         }
     });*/
     
-    
+    //首次執行，設定桃園市
     City="Taoyuan";
     $("#SelectCity option[value=" + City + "]").attr("selected", "true");
     getRouteData();
@@ -43,29 +47,42 @@ $(function () {
 
 //url = "https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/" + City + "/" + RouteName + "?$filter=RouteName%2FZh_tw%20eq%20'" + RouteName + "'&$format=JSON";
 
-function getRouteData() {
-    console.log(City + ' ' + RouteName);
-    if(City == "InterCity") {
-        if(RouteName == ''){
-            url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/InterCity?$format=JSON";
+//模糊搜尋路線
+function searchRouteData() {
+    var RouteData = $("#RouteData");
+    $.each(RouteData.children() ,function(index, element) {
+        if($(element).attr("value").search(RouteName) == -1){
+            $(element).hide();
         }else{
-            url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/InterCity/" + RouteName + "?$format=JSON";
+            $(element).show();
         }
+    });
+}
+
+//取得所有路線資料
+function getRouteData() {
+    if(City == "InterCity") {
+        url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/InterCity?$format=JSON";
     }
-    else if(RouteName == '') {
+    else{
         url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/" + City + "?$format=JSON";
     }
-    else {
-        url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/" + City + "/" + RouteName + "?$format=JSON";
-    }
+    
+    //抓取JSON資料並印出
     $.getJSON(url, function(JSONData) {
         Data = sortJSONData(JSONData, "RouteName", "Zh_tw");
         $.each(Data, function(index, element) {
-            $("#RouteData").append('<a href="#" class="list-group-item link-color">' + element["RouteName"]["Zh_tw"] + "<br>" + element["DepartureStopNameZh"] + "->" + element["DestinationStopNameZh"] + '</a>');
+            //部分縣市Headsign欄位可能為空，需要判斷
+            if(element["SubRoutes"][0]["Headsign"] == undefined) {
+                $("#RouteData").append('<a href="#" value="' + element["RouteName"]["Zh_tw"] + '" class="list-group-item link-color">' + element["RouteName"]["Zh_tw"] + "<br>" + element["DepartureStopNameZh"] + ' - ' + element["DestinationStopNameZh"] + '</a>');
+            }else {
+                $("#RouteData").append('<a href="#" value="' + element["RouteName"]["Zh_tw"] + '" class="list-group-item link-color">' + element["RouteName"]["Zh_tw"] + "<br>" + element["SubRoutes"][0]["Headsign"] + '</a>');
+            }
         });
     });
 }
 
+//資料排序
 function sortJSONData(JSONData, key, key2) {
     return JSONData.sort(function(a, b) {
         var x, y;
@@ -75,32 +92,4 @@ function sortJSONData(JSONData, key, key2) {
             x = a[key][key2], y = b[key][key2];
         return ((x < y) ? -1 : ((x > y) ? 1 : 0));
     });
-}
-
-/*
-$(function () {
-    $.ajax({
-        type: 'GET',
-        url: 'https://ptx.transportdata.tw/MOTC/v2/Rail/TRA/Station?$top=10&$format=JSON',
-        dataType: 'json',
-        headers: GetAuthorizationHeader(),
-        success: function (Data) {
-            $('body').text(JSON.stringify(Data));
-        }
-    });
-});
-*/
-
-function GetAuthorizationHeader() {
-    var AppID = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
-    var AppKey = 'FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF';
-
-    var GMTString = new Date().toGMTString();
-    var ShaObj = new jsSHA('SHA-1', 'TEXT');
-    ShaObj.setHMACKey(AppKey, 'TEXT');
-    ShaObj.update('x-date: ' + GMTString);
-    var HMAC = ShaObj.getHMAC('B64');
-    var Authorization = 'hmac username=\"' + AppID + '\", algorithm=\"hmac-sha1\", headers=\"x-date\", signature=\"' + HMAC + '\"';
-
-    return { 'Authorization': Authorization, 'X-Date': GMTString /*,'Accept-Encoding': 'gzip'*/};
 }
