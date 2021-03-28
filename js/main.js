d0396f3f212fea3ef19452a2e4284554b50fd245
molrobot <john19990708@gmail.com>
2021-03-28 21:46:51 +0800

    Use IndexedDB to save bus route list

--- js/main.js
+++ js/main.js
@@ -2,6 +2,12 @@ $(function () {
     // 城市名稱、路線名稱
     let city, routeName;
 
+    const DB_NAME = "bus_data";
+    const DB_VERSION = 2;
+    const DB_BUS_LIST = "bus_route_list"
+
+    let db;
+
     // 下拉式選單新增城市名稱
     $.each(CityList, function (index, item) {
         $("#SelectCity").append($('<option>', {
@@ -16,7 +22,15 @@ $(function () {
         $("input#RouteName").val("");
         routeName = "";
         $("#RouteData").empty();
-        getRouteData(city);
+
+        openDatabase().then(function (result) {
+            console.log(result);
+            return getRouteList();
+        }).then(function (routeList) {
+            console.assert(routeList !== undefined && routeList[0] !== undefined);
+            console.log(routeList);
+            printRouteList(routeList);
+        });
     });
 
     // 模糊搜尋輸入事件
@@ -29,25 +43,79 @@ $(function () {
     // 首次執行，設定桃園市
     city = "Taoyuan";
     $("#SelectCity option[value=" + city + "]").attr("selected", "true");
-    getRouteData(city);
-});
+    $("#SelectCity").trigger("change");
 
-// 取得所有路線資料
-function getRouteData(city) {
-    let url;
-    if(city === "InterCity") {
-        url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/InterCity?$format=JSON";
-    }
-    else{
-        url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/" + city + "?$format=JSON";
+    function openDatabase() {
+        let $d = $.Deferred();
+        let request = indexedDB.open(DB_NAME, DB_VERSION)
+        request.onsuccess = function (event) {
+            db = this.result;
+            $d.resolve(this.result);
+        }
+        request.onerror = function (event) {
+            $d.resolve(event.target.errorCode);
+        }
+        request.onupgradeneeded = function (event) {
+            console.log("onupgradeneeded");
+            event.currentTarget.result.createObjectStore(
+                DB_BUS_LIST, {keyPath: "city", autoIncrement: true});
+        }
+        return $d;
     }
 
-    // 抓取JSON資料並印出
-    $.when(getJSONData(url)).done(function (JSONData) {
-        // console.log(JSONData);
-        let routeListData = sortJSONData(JSONData), headsign;
-        $.each(routeListData, function(index, element) {
-            let routeName = element["RouteName"]["Zh_tw"];
+    function getObjectStore(storeName, mode) {
+        let tx = db.transaction(storeName, mode);
+        return tx.objectStore(storeName);
+    }
+
+    // 取得所有路線資料
+    function getRouteList() {
+        let $d = $.Deferred();
+        let routeList = {};
+
+        let objectStore = getObjectStore(DB_BUS_LIST, "readwrite");
+        let request = objectStore.get(city);
+        request.onsuccess = function (event) {
+            routeList = event.target.result;
+            if(routeList === undefined){
+                routeList = saveRouteList();
+            } else {
+                routeList = routeList["data"];
+            }
+            $d.resolve(routeList);
+        };
+        request.onerror = function (event) {
+            $d.reject(event.target.result + "路線資料取得失敗");
+        }
+        return $d;
+    }
+
+    function saveRouteList() {
+        let url;
+        if(city === "InterCity") {
+            url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/InterCity?$format=JSON";
+        }
+        else{
+            url = "https://ptx.transportdata.tw/MOTC/v2/Bus/Route/City/" + city + "?$format=JSON";
+        }
+        return $.when(getJSONData(url)).done(function (JSONData) {
+            // console.log(JSONData);
+            let routeList = sortJSONData(JSONData);
+            let objectStore = getObjectStore(DB_BUS_LIST, "readwrite");
+            let request = objectStore.add({city: city, data: routeList});
+            request.onsuccess = function (event) {
+                console.log(event.target.result + "路線資料更新成功");
+            }
+            request.onerror = function (event) {
+                console.log(event.target.result + "路線資料更新失敗");
+            }
+            return routeList;
+        });
+    }
+
+    function printRouteList(routeList) {
+        $.each(routeList, function(index, element) {
+            let routeName = element["RouteName"]["Zh_tw"], headsign;
             let link = "bus.html?city=" + city + "&route=" + routeName;
             // console.log(link);
 
@@ -58,26 +126,26 @@ function getRouteData(city) {
                 headsign = element["SubRoutes"][0]["Headsign"];
             $("#RouteData").append('<a target="_bus" href="' + link + '" class="list-group-item link-color">' + routeName + "<br>" + headsign + "</a>");
         });
-    });
-}
+    }
 
-// 模糊搜尋路線
-function searchRouteData(routeName) {
-    $.each($("#RouteData").children() ,function(index, item) {
-        if($(item).text().indexOf(routeName) === -1){
-            $(item).hide();
-        }else{
-            $(item).show();
-        }
-    });
-}
+    // 模糊搜尋路線
+    function searchRouteData() {
+        $.each($("#RouteData").children() ,function(index, item) {
+            if($(item).text().indexOf(routeName) === -1){
+                $(item).hide();
+            }else{
+                $(item).show();
+            }
+        });
+    }
 
-// 資料排序
-function sortJSONData(JSONData) {
-    return JSONData.sort(function(a, b) {
-        let x, y;
-        x = a["RouteName"]["Zh_tw"];
-        y = b["RouteName"]["Zh_tw"];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-}
+    // 資料排序
+    function sortJSONData(JSONData) {
+        return JSONData.sort(function(a, b) {
+            let x, y;
+            x = a["RouteName"]["Zh_tw"];
+            y = b["RouteName"]["Zh_tw"];
+            return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+        });
+    }
+});
